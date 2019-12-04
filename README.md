018b708f1bc54b4d4ad5393c7834ad542785ccb8
Gustavo nRm <gustavonrm@gmail.com>
2019-12-04 18:27:48 +0000

    stuff

---
+++ README.md
@@ -0,0 +1,1 @@
+RCOM Proj 2 
---
+++ scripts/tux54.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+/etc/init.d/networking restart
+ifconfig eth0 up
+ifconfig eth0 172.16.50.254/24
+ifconfig eth1 up
+ifconfig eth1 172.16.51.253/24
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
